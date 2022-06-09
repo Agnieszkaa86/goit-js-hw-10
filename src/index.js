20d091b2418c34edb780dd282f9101a5e47ce6af
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-06-09 20:39:48 +0200

    add file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
-const serchBox = document.querySelector('#search-box');
+const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.ciuntry-info');
+
+searchBox.addEventListener('input', () => {
+  fetchUsers()
+    .then(users => renderUserList(users))
+    .catch(error => console.log(error));
+});
+
+/*function fetchUsers() {
+  return fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+});
+}*/
+
+function renderUserList(users) {
+  const markup = users
+    .map(user => {
+      return `<li>
+          <p><b>Name</b>: ${user.name}</p>
+          <p><b>Email</b>: ${user.email}</p>
+          <p><b>Company</b>: ${user.company.name}</p>
+        </li>`;
+    })
+    .join('');
+  userList.innerHTML = markup;
+}
