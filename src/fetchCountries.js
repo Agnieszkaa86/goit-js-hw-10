8ea4f4d19302ea95c148c1efaa5458fea4206916
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-06-13 16:56:03 +0200

    add task 10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,15 @@
 export function fetchCountries(name) {
-    fetch('https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages',)
+    return fetch
+        (`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`)
+        
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
             }
-            console.log(response);
-            return response.json();
-        })
-  
-        .catch(error => console.log(err))
-}
+            
+        console.log(response);
+        return response.json();
+        }) 
+        .catch (err => console.log(err));
+    }
+
--- src/index.js
+++ src/index.js
@@ -6,32 +6,70 @@ var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.ciuntry-info');
+const countryInfo = document.querySelector('.country-info');
 
-searchBox.addEventListener('input', () => {
-  fetchUsers()
-    .then(users => renderUserList(users))
-    .catch(error => console.log(error));
-});
+searchBox.addEventListener('input',
+  debounce(searchCountry, DEBOUNCE_DELAY)
+);
 
-/*function fetchUsers() {
-  return fetch('https://jsonplaceholder.typicode.com/users').then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-});
-}*/
-
-function renderUserList(users) {
-  const markup = users
-    .map(user => {
-      return `<li>
-          <p><b>Name</b>: ${user.name}</p>
-          <p><b>Email</b>: ${user.email}</p>
-          <p><b>Company</b>: ${user.company.name}</p>
-        </li>`;
+function searchCountry () {
+  let name = searchBox.value;
+  if (name === '') {
+    Notiflix.Notify.info('Please type a country name');
+    resetList();
+  } else {
+    fetchCountries(name.trim())
+      .then(countries => renderCountryList(countries))
+      .catch(() => {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+        resetList();
+      });
+  }
+}
+function renderCountryList(countries) {
+  if (countries.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    resetList();
+  } else if (countries.length <= 10 && countries.length >= 2) {
+    resetList();
+      
+    const markup = countries
+      .map(({ name, flags }) => {
+        return `<li class= "country_list"><img class= "flag_img" src= "${flags.svg}">
+                <h1>${name}</h1>
+                </li>`;
     })
     .join('');
-  userList.innerHTML = markup;
+  countryList.innerHTML = markup;
+  }else if (countries.length === 1){
+    resetList();
+    const markup = countries
+      .map(({ name, flags,capital, population, languages }) => {
+        return `< div class = "main" >
+          <img class = "div_flag" src = "${flags.svg}">
+            <h2>${name}</h2>
+            </div>
+         < li class= "country_list" >
+          </li>
+          <p><b>Capital</b>: ${capital}</p>
+           </li>
+         < li class= "country_list" >
+          <p><b>Population</b>: ${population}</p> 
+          </li>
+         < li class= "country_list" >
+          <p>Languages</b>: ${languages.map(({name}) => ' ' + name )}</p>
+           </li >`;
+      })
+      .join('');
+countryInfo.innerHTML = markup;
+  }
 }
+const resetList = () => {
+  countryInfo.innerHTML = '';
+  countryList.innerHTML = '';
+};
+
+
+
